6fd7477ce9dedbd7550115161c4b58088ca04fce
QianYizhou <qianyizhou17@gmail.com>
2024-05-21 16:50:01 +0800

    add help scripts; update submodule

---
+++ yizhou_setup_build_env.sh
@@ -0,0 +1,13 @@
+# !/bin/bash
+
+# Intall *boost-dev*:
+sudo apt install libboost-dev libboost-serialization-dev libboost-system-dev libboost-filesystem-dev libboost-program-options-dev libboost-thread-dev -y
+
+# Install *ruby-dev*: 
+sudo apt install ruby-dev -y
+
+# Install *libxml-xpath-perl*:
+sudo apt install libxml-xpath-perl -y
+
+# To build LuaComponent with Lua library
+sudo apt-get install liblua5.1-0-dev -y
